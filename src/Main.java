dbca66ff0e32c543a198a53eba1770d0161d580e
mohammed <mohammedre4a@gmail.com>
2022-10-18 12:10:43 +0200

    feat: add notes.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
     }
 }
+/*
+    Store and operate on Items: HashMap<Title, item>
+    Store locally -> files.
+    Fetch and operate on items using the Map.
+    Before closing the application, save the map to file.
+    Save items as Json
+    {
+        first, 'title'
+        this is first, 'description'
+    }
+* */
