a6fd6b94a4229f8a34e7d866390d00032b76d0bb
mohammed yasser <47453799+mohammedysamir@users.noreply.github.com>
2022-10-18 13:01:49 +0200

    Merge pull request #1
    * feat: added TodoItem model and Priorities enum.

--- src/TodoItem.java
+++ src/TodoItem.java
@@ -1,2 +1,74 @@
+import java.util.Date;
+
 public class TodoItem {
+    String title,description,category;
+    Date startDate,endDate;
+    Boolean isFavorite;
+    Priorities priority;
+
+    public TodoItem(String title, String description, String category, Date startDate, Date endDate, Boolean isFavorite, Priorities priority) {
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.isFavorite = isFavorite;
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Boolean getFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(Boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public Priorities getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priorities priority) {
+        this.priority = priority;
+    }
 }
