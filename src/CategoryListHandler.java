a4c3d46063d91f46b2332d691794767e002f0f0d
mohammed <mohammedre4a@gmail.com>
2022-10-19 11:27:44 +0200

    feat: added class to handle categories to a file.

---
+++ src/CategoryListHandler.java
@@ -0,0 +1,38 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CategoryListHandler {
+    ArrayList<String> categories = new ArrayList<>();
+    private static final String categoriesPath = "categories";
+
+    public CategoryListHandler() throws FileNotFoundException {
+        //open category file
+        FileHandler fileHandler = new FileHandler();
+        //parse categories into Array and set it
+        categories = parseCategories(fileHandler.readFromFile(categoriesPath).toString());
+    }
+
+    public void addCategory(String category) {
+        categories.add(category);
+    }
+
+    public boolean isCategoryFound(String category) {
+        return categories.contains(category);
+    }
+
+    private ArrayList<String> parseCategories(String categoriesString) {
+        return (ArrayList<String>) Arrays.asList(categoriesString.split("\r\n"));
+    }
+
+    public void writeCategoriesToFile() throws IOException {
+        StringBuilder result = new StringBuilder();
+        for (String category : categories)
+            result.append(category + "\n");
+        Files.writeString(Path.of(categoriesPath), result.toString());
+    }
+
+}
